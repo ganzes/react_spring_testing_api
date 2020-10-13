6f5e35a22997331b15151c205f91da0110acafdd
ganzes <charczuk.arkadiusz@gmail.com>
2020-10-14 01:45:51 +0200

    U_001_added_Event_Group_User_classes

---
+++ src/main/java/com/ganzes/testing_learning/jugtours/model/Group.java
@@ -0,0 +1,32 @@
+package com.ganzes.testing_learning.jugtours.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "user_group")
+public class Group {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private String name;
+    private String address;
+    private String city;
+    private String stateOrProvince;
+    private String postalCode;
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private User user;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<Event> events;
+}
