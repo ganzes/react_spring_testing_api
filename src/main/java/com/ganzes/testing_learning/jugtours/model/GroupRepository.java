571f8b90381a2e6ef3b83fbeb85f534c223ac443
ganzes <charczuk.arkadiusz@gmail.com>
2020-10-14 02:16:54 +0200

    U_002_added_Initializer

---
+++ src/main/java/com/ganzes/testing_learning/jugtours/model/GroupRepository.java
@@ -0,0 +1,8 @@
+package com.ganzes.testing_learning.jugtours.model;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+    Group findByName(String name);
+}
